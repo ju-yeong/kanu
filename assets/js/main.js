7874a9bf4b044655f44cd053ad766886e1be6845
ju yeong <87563115+ju-yeong@users.noreply.github.com>
2021-11-02 21:26:18 +0900

    kanu

---
+++ assets/js/main.js
@@ -0,0 +1,149 @@
+$('.btn').click(function(){
+    $('.gnb').slideToggle()
+})
+
+
+
+// section03 animation
+
+$(function(){
+    $(".s03-div1").hover(function(){
+        $(".s03-img1").css("opacity","1");
+        $(".s03-div1>p").css("opacity","1");
+    },function(){
+        $(".s03-img1").css("opacity","0").css("transition-delay",".05s");
+        $(".s03-div1>p").css("opacity","0");
+    });
+});
+
+$(function(){
+    $(".s03-div2").hover(function(){
+        $(".s03-img2").css("opacity","1");
+        $(".s03-div2>p").css("opacity","1");
+    },function(){
+        $(".s03-img2").css("opacity","0").css("transition-delay",".05s");
+        $(".s03-div2>p").css("opacity","0");
+    });
+});
+
+$(function(){
+    $(".s03-div3").hover(function(){
+        $(".s03-img3").css("opacity","1");
+        $(".s03-div3>p").css("opacity","1");
+    },function(){
+        $(".s03-img3").css("opacity","0").css("transition-delay",".05s");
+        $(".s03-div3>p").css("opacity","0");
+    });
+});
+
+$(function(){
+    $(".s03-div4").hover(function(){
+        $(".s03-img4").css("opacity","1");
+        $(".s03-div4>p").css("opacity","1");
+    },function(){
+        $(".s03-img4").css("opacity","0").css("transition-delay",".05s");
+        $(".s03-div4>p").css("opacity","0");
+    });
+});
+
+$(function(){
+    $(".s03-div5").hover(function(){
+        $(".s03-img5").css("opacity","1");
+        $(".s03-div5>p").css("opacity","1");
+    },function(){
+        $(".s03-img5").css("opacity","0").css("transition-delay",".05s");
+        $(".s03-div5>p").css("opacity","0");
+    });
+});
+
+$(function(){
+    $(".s03-div6").hover(function(){
+        $(".s03-img6").css("opacity","1");
+        $(".s03-div6>p").css("opacity","1");
+    },function(){
+        $(".s03-img6").css("opacity","0").css("transition-delay",".05s");
+        $(".s03-div6>p").css("opacity","0");
+    });
+});
+
+$(function(){
+    $(".s03-div7").hover(function(){
+        $(".s03-img7").css("opacity","1");
+        $(".s03-div7>p").css("opacity","1");
+    },function(){
+        $(".s03-img7").css("opacity","0").css("transition-delay",".05s");
+        $(".s03-div7>p").css("opacity","0");
+    });
+});
+
+$(function(){
+    $(".s03-div8").hover(function(){
+        $(".s03-img8").css("opacity","1");
+        $(".s03-div8>p").css("opacity","1");
+    },function(){
+        $(".s03-img8").css("opacity","0").css("transition-delay",".05s");
+        $(".s03-div8>p").css("opacity","0");
+    });
+});
+
+// section03 animation
+
+// section04 hover
+
+$(function(){
+    $(".thi-1").hover(function(){
+        $(".thi-1>img").css("opacity","0");
+    },function(){
+        $(".thi-1>img").css("opacity","1");
+    });
+}),
+
+$(function(){
+    $(".thi-2").hover(function(){
+        $(".thi-2>img").css("opacity","0");
+    },function(){
+        $(".thi-2>img").css("opacity","1");
+    });
+});
+
+$(function(){
+    $(".thi-3").hover(function(){
+        $(".thi-3>img").css("opacity","0");
+    },function(){
+        $(".thi-3>img").css("opacity","1");
+    });
+});
+
+// section04 hover end
+
+$(".thi-1").click(function(){
+    $(".modal-pop-01").fadeIn();
+})
+$(document).mouseup(function(){
+    $(".modal-pop-01").fadeOut();
+})
+$(".thi-2").click(function(){
+    $(".modal-pop-02").fadeIn();
+})
+$(document).mouseup(function(){
+    $(".modal-pop-02").fadeOut();
+})
+$(".thi-3").click(function(){
+    $(".modal-pop-03").fadeIn();
+})
+$(document).mouseup(function(){
+    $(".modal-pop-03").fadeOut();
+})
+
+// section04 modal end
+
+// $('.sec05-w').slick({
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+// });
+
+$('.sec05-slider').slick();
+
+// $(function(){
+//     $("p").css("background-color","blue");
+// });
---
+++ assets/js/study.js
@@ -0,0 +1,41 @@
+// let num=1.4184;
+
+// let maxNum=Math.max(10,20,30,40,50);
+// document.write(maxNum+'<br>');
+// let minNum=Math.min(10,20,30,40,50);
+// document.write(minNum+'<br>');
+// let roundNum=Math.round(3.14);
+// document.write(roundNum+'<br>')
+
+// let randomNum=Math.random();
+// document.write(randomNum+'<br>');
+
+// let piNum=Math.PI;
+// document.write(piNum+'<br>');
+
+// //1
+// let result = Math.ceil(Math.random()*100);
+// document.write(result+'<br>');
+// //2
+// let lottoNum=Math.random();
+// let lottoResult=Math.ceil(lottoNum*100);
+// document.write(lottoResult+'<br>');
+
+
+let lottoNum1 = Math.ceil(Math.random()*100);
+document.querySelector('.lotto01').innerHTML = lottoNum1;
+
+let lottoNum2 = Math.ceil(Math.random()*100);
+document.querySelector('.lotto02').innerHTML = lottoNum2;
+
+let lottoNum3 = Math.ceil(Math.random()*100);
+document.querySelector('.lotto03').innerHTML = lottoNum3;
+
+let lottoNum4 = Math.ceil(Math.random()*100);
+document.querySelector('.lotto04').innerHTML = lottoNum4;
+
+let lottoNum5 = Math.ceil(Math.random()*100);
+document.querySelector('.lotto05').innerHTML = lottoNum5;
+
+let lottoNum6 = Math.ceil(Math.random()*100);
+document.querySelector('.lotto06').innerHTML = lottoNum6;
